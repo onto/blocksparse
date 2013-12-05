f658a89fbfa0ede61041f0a123011d3e04fec66c
Anton Lashkov <lenton_91@mail.ru>
2013-12-05 12:13:43 +0400

    +

--- decompositor.h
+++ decompositor.h
@@ -42,17 +42,11 @@ public:
 
 private:
     std::vector< std::vector<size_t> > H;
-    std::vector<size_t> G;
     size_t N;
-    std::vector< Domain > D;
+    std::vector< std::vector<size_t> > D;
+    std::vector<bool> U, I;
 
-    std::vector<size_t> colorByBFS(size_t start, size_t color);
-    void recolorGroup(Domain &Dt);
-    void colorGroups();
-    void moveNodeToInterface(size_t index, Domain &Dt);
     void moveNodeToInterface(size_t index);
-    void decomposeMy();
-    void decomposeDP(size_t M = 10);
     void decomposeSangiovanniVincentelli();
     void printH();
 };
@@ -85,23 +79,21 @@ MatrixDecompositor::MatrixDecompositor(const char *file)
     }
     in.close();
 
-    //decomposeMy();
-    //decomposeDP();
     decomposeSangiovanniVincentelli();
 
     //Заполним структуру результата
     Pct.push_back(0);
     for (size_t i = 1; i < D.size(); ++i)
     {
-        if (D[i].nodes.size() == 0) continue;
-        Pct.insert(Pct.end(), D[i].nodes.begin(), D[i].nodes.end());
-        R.push_back(D[i].nodes.size());
+        if (D[i].size() == 0) continue;
+        Pct.insert(Pct.end(), D[i].begin(), D[i].end());
+        R.push_back(D[i].size());
     }
 
-    if (D[0].nodes.size() != 0)
+    if (D[0].size() != 0)
     {
-        Pct.insert(Pct.end(), D[0].nodes.begin(), D[0].nodes.end());
-        R.push_back(D[0].nodes.size());
+        Pct.insert(Pct.end(), D[0].begin(), D[0].end());
+        R.push_back(D[0].size());
     }
 
     Pr.resize(N+1);
@@ -123,84 +115,9 @@ MatrixDecompositor::MatrixDecompositor(const char *file)
 
     //Освободим память
     H.clear();
-    G.clear();
     D.clear();
-}
-
-std::vector<size_t> MatrixDecompositor::colorByBFS(size_t start, size_t color)
-{
-    std::queue<size_t> nodes;
-    std::vector<size_t> dnodes;
-    size_t cur, t;
-
-    nodes.push(start);
-    G[start] = color;
-
-    while (!nodes.empty())
-    {
-        cur = nodes.front();
-        nodes.pop();
-
-        dnodes.push_back(cur);
-
-        for (size_t i = 0; i < H[cur].size(); ++i)
-        {
-            t = H[cur][i];
-            if (G[t] == 0)
-            {
-                nodes.push(t);
-                G[t] = color;
-            }
-        }
-    }
-
-    return dnodes;
-}
-
-void MatrixDecompositor::recolorGroup(Domain &Dt)
-// Перекраска группы
-{
-    size_t N = Dt.nodes.size();
-    size_t color = Dt.index;
-
-    std::vector<size_t> nodesForRecol = Dt.nodes; //Запомним узлы, которые нужно перекрасить
-
-    for (size_t k = 0; k < N; ++k)
-        G[nodesForRecol[k]] = 0; //Сбросим индексы групп
-
-    Dt.nodes = colorByBFS(nodesForRecol[0], color); //Раскрасим начиная с первого в группе
-
-    color = D.size()-1;
-    //Теперь если остались не раскрашенные, то раскрасим их
-    size_t cur;
-    for (size_t k = 1; k < N; ++k)
-    {
-        cur = nodesForRecol[k];
-        if (G[cur] == 0)
-        {
-            ++color;
-            D.push_back( Domain(color, colorByBFS(cur, color)) );
-        }
-    }
-}
-
-void MatrixDecompositor::colorGroups()
-// Начальное разбиение на группы
-{
-    size_t color = 0; //Индекс текущей группы (группы будут начинаться с 1)
-    G.assign(N, 0); //Сбросим индексы групп
-    for (size_t k = 0; k < N; ++k)
-    {
-        if (G[k] == 0)
-            //Если еще не занесен в группу
-            //то делаем поиск в ширину и помечаем элементы текущим индексом
-        {
-            ++color;
-
-            //std::vector<size_t> nodes = colorByBFS(k, color);
-            D.push_back( Domain(color, colorByBFS(k, color)) );
-        }
-    }
+    U.clear();
+    I.clear();
 }
 
 void MatrixDecompositor::moveNodeToInterface(size_t index)
@@ -216,185 +133,8 @@ void MatrixDecompositor::moveNodeToInterface(size_t index)
             H[cur].erase(it);
     }
     H[index].clear();
-    G[index] = 0;
-    D[0].nodes.push_back(index);
-}
-
-void MatrixDecompositor::moveNodeToInterface(size_t index, Domain& Dt)
-{
-    moveNodeToInterface(index);
-
-    std::vector<size_t>::iterator it = std::find(Dt.nodes.begin(),
-                                                 Dt.nodes.end(), index);
-    if (it != Dt.nodes.end())
-        Dt.nodes.erase(it);
-}
-
-void MatrixDecompositor::decomposeMy()
-{
-    std::vector< std::pair<size_t,size_t> > LS;  //Количество связей элементов
-                                                 //в виде пары, чтобы можно было сортировать
-    std::vector<bool> nodeFlags(N, true);
-    D.clear();
-    D.push_back( Domain(0, std::vector<size_t>()) );
-
-    //Начальное определение групп
-    colorGroups();
-
-    bool nothingtoremove;
-
-    while(true)
-    {
-        nothingtoremove = true;
-
-        //Отсортируем группы по размеру по убыванию
-        std::sort(D.begin()+1, D.end(), SortHelper::compareDomainSize);
-
-        size_t M = D.size();
-
-        double F = 0;
-        for (size_t i = 1; i < M; ++i)
-            F += pow(D[i].nodes.size(), 3.);
-        F = pow(F, 1./3.);
-
-        if (D[0].nodes.size() >= F) break;
-
-        //Теперь последовательно пытаемся разделить самую большую группу
-        for (size_t i = 1; i < M; ++i)
-        {
-            //Найдем количество связей
-            LS.clear();
-            for (size_t q = 0; q < D[i].nodes.size(); ++q)
-            {
-                size_t node = D[i].nodes[q];
-                LS.push_back( std::pair<size_t,size_t>( node, H[node].size() ) );
-            }
-
-            //Отсортируем количество связей по убыванию
-            std::sort(LS.begin(), LS.end(), SortHelper::comaprePairSecond);
-
-            bool success = false;
-            for (size_t j = 0; j < LS.size(); ++j)
-            {
-                //Теперь последовательно пробуем удалить элемент
-                //с индексом LS[j].first
-                //Он не должен оставлять элемент без связей
-                //Если такого не найдется то выходим
-                //
-                size_t index = LS[j].first;
-                if (!nodeFlags[index]) continue;
-
-                //Проверим на то, что он не оставит элементов без связей
-                bool goodNode = true;
-                for (size_t k = 0; k < H[index].size(); ++k)
-                    if (H[ H[index][k] ].size() <= 1)
-                    {
-                        goodNode = false;
-                        break;
-                    }
-
-                if (!goodNode)
-                {
-                    nodeFlags[index] = false;
-                    continue;
-                }
-
-                //Убираем элемент
-                moveNodeToInterface(index, D[i]);
-                //И перекрашиваем группу, вдруг разделилась
-                recolorGroup(D[i]);
-                success = true;
-                break;
-            }
-
-            if (success)
-            {
-                nothingtoremove = false;
-                break;
-            }
-        }
-
-        if (nothingtoremove) break;
-    }
-}
-
-void MatrixDecompositor::decomposeDP(size_t M)
-{
-    D.clear();
-    D.push_back( Domain(0, std::vector<size_t>()) );
-
-    G.assign(N, 0);
-
-    //Начальное определение групп
-    //colorGroups();
-
-    srand(time(NULL));
-
-    std::vector<size_t> V;
-
-    M = std::max(2.,(log10(N*1.) - 1.) * 10.);
-
-    std::vector< std::pair<size_t,size_t> > LS;
-
-    for (size_t i = 0; i < H.size(); ++i)
-        LS.push_back( std::pair<size_t,size_t>( i, H[i].size() ) );
-
-    std::sort(LS.begin(), LS.end(), SortHelper::comaprePairSecondInv);
-
-    for (size_t i = 0; i < M; ++i)
-    {
-        //size_t q = rand()%(N/M) + i*(N/M);
-        size_t q = LS[i].first;
-        V.push_back(q);
-        G[q] = i+1;
-        D.push_back( Domain(i+1, std::vector<size_t>(1, q)) );
-    }
-
-    size_t nNodes = M;
-
-    //while (nNodes < N)
-    //{
-        for (size_t vi = 0; vi < V.size(); ++vi)
-        {
-            size_t i = V[vi];
-            size_t color = G[i];
-
-            if (color == M+1) continue; //Вершина из интерфейса
-
-            for (size_t j = 0; j < H[i].size(); ++j) //Красим соседей
-            {
-                size_t cur = H[i][j];
-
-                if (G[cur] == color) continue; //Если цвет уже такой же, идем дальше
-                if (G[cur] == M+1) continue; //Вершина из интерфейса, идем дальше
-
-                if (G[cur] == 0) //Если не раскрашена, красим в текущий цвет
-                {
-                    G[cur] = color; //Красим
-                    D[color].nodes.push_back(cur); //Добавляем в группу
-                    ++nNodes;
-                    V.push_back(cur);
-                }
-                else if (G[cur] != M+1) //Если помечена другим цветом, то переносим в интефейс
-                {
-                    D[0].nodes.push_back(cur);
-
-                    for (size_t k = 0; k < D[G[cur]].nodes.size(); ++k)
-                        if (D[G[cur]].nodes[k] == cur)
-                        {
-                            D[G[cur]].nodes.erase(D[G[cur]].nodes.begin()+k);
-                            break;
-                        }
-
-                    G[cur] = M+1;
-                }
-            }
-        }
-    //}
-    D.push_back( Domain(M+1, std::vector<size_t>()) );
-    for (size_t i = 0; i < N; ++i)
-        if (G[i] == 0)
-            D[M+1].nodes.push_back(i);
+    I[index] = true; U[index] = true;
+    D[0].push_back(index);
 }
 
 void MatrixDecompositor::decomposeSangiovanniVincentelli()
@@ -403,11 +143,12 @@ void MatrixDecompositor::decomposeSangiovanniVincentelli()
     std::vector<size_t> is;
     std::vector<size_t> asn, asp;
 
-    G.assign(N,N);
+    U.assign(N,false);
+    I.assign(N,false);
 
     D.clear();
-    D.push_back( Domain(0, std::vector<size_t>()) );
-    D.push_back( Domain(1, std::vector<size_t>()) );
+    D.push_back( std::vector<size_t>() );
+    D.push_back( std::vector<size_t>() );
 
     //Вот это определим сами
     size_t Nmax = N / 10.;
@@ -429,8 +170,8 @@ void MatrixDecompositor::decomposeSangiovanniVincentelli()
     asn = H[in];
     cn.push_back(asmin);
     is.push_back(in);
-    D.back().nodes.push_back(in);
-    G[in] = 1;
+    D.back().push_back(in);
+    U[in] = true;
 
     for (size_t i = 1; i < N; ++i)
     {
@@ -443,14 +184,14 @@ void MatrixDecompositor::decomposeSangiovanniVincentelli()
             in = 0, asmin = ULONG_MAX;
             for (size_t i = 0; i < N; ++i)
             {
-                if ((G[i] == N) && (H[i].size() < asmin))
+                if ((!U[i]) && (H[i].size() < asmin))
                 {
                     asmin = H[i].size();
                     in = i;
                 }
             }
             asn = H[in];
-            D.push_back( Domain(1, std::vector<size_t>()) );
+            D.push_back( std::vector<size_t>() );
         }
         else
         {
@@ -475,12 +216,11 @@ void MatrixDecompositor::decomposeSangiovanniVincentelli()
                 for (size_t k = 0; k < H[cur].size(); ++k)
                 {
                     size_t adj = H[cur][k];
-                    if ((std::find(asp.begin(),asp.end(), adj) == asp.end()) &&
-                        (G[adj] == N))
+                    if ((!U[adj]) &&
+                        (std::find(asp.begin(),asp.end(), adj) == asp.end()))
                     {
                         asadd.push_back(adj);
                     }
-
                 }
 
 #pragma omp critical
@@ -502,23 +242,23 @@ void MatrixDecompositor::decomposeSangiovanniVincentelli()
 
         //Вроде как определение локального минимума
         //И тут перенос в интерфейс всех узлов из AS(i-1)
-        if ((D.back().nodes.size() > 0.75*Nmax) && (cn[i-2] >= cn[i-1]) && (cn[i-1] < cn[i]))
+        if ((D.back().size() > 0.75*Nmax) && (cn[i-2] >= cn[i-1]) && (cn[i-1] < cn[i]))
         {
             for (size_t j = 0; j < asp.size(); ++j)
             {
                 size_t rem = asp[j];
-                if (G[rem] == 0) continue;
+                if (I[rem]) continue;
                 moveNodeToInterface(rem);
             }
 
-            D.push_back( Domain(1, std::vector<size_t>()) );
+            D.push_back( std::vector<size_t>() );
         }
         else
         {
-            if (G[in] != 0)
+            if (!I[in])
             {
-                D.back().nodes.push_back(in);
-                G[in] = 1;
+                D.back().push_back(in);
+                U[in] = true;
             }
         }
     }
